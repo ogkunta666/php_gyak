3cc8412ae1f2d7ceeb811606a75cc1f31fb9e00e
ogkunta666 <markizoli06@gmail.com>
2025-09-04 11:21:55 +0200

    variables.php kesz

--- variables.php
+++ variables.php
@@ -20,4 +20,32 @@ echo "<br>";
 echo "név: " . $name . "<br>";
 echo "kor: " . $age . "<br>";
 echo "város: " . $city . "<br>";
+
+// Konstansok
+define("PI", 3.14);
+echo "PI értéke: " . PI . "<br>";
+
+// Adattípusok : string, integer, float, boolean, array, object, null
+
+$message = "1";
+echo "Kiír", $message . "értéket.", "<br>\n"; 
+echo "Kiír $message értéket.", "<br>\n";
+
+
+/*
+ git parancsok:
+    git init - új git helyi repository létrehozása
+    git status - aktuális állapot megtekintése
+    git add . - fájlok hozzáadása a staging area-hoz
+    git commit -m "commit üzenet" - változtatások elkötelezése
+    git log - commitok listázása
+    git branch -m main - főág mainre nevezése
+    git checkout ág_név - átváltás egy másik ágra
+    git merge ág_név - ágak összeolvasztása
+    git remote add origin távoli_repo_url - távoli repository hozzáadása
+    git push origin ág_név - változtatások feltöltése a távoli repository-ba
+    git pull origin ág_név - változtatások lehúzása a távoli repository-ból
+
+    hf : toltsd le otthon a repot a sajat htdocs konyvtaradba
+*/
 ?>
