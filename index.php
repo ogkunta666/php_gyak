3cb7841de80fc7acc74afda292372eb7d1c5588c
ogkunta666 <markizoli06@gmail.com>
2025-09-11 11:04:55 +0200

    index php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-echo "Szia";
+require 'app/Traits/LoggerTrait.php';
+require 'app/Traits/GreetingTrait.php';
+require 'app/Services/MyService.php';
 
+use App\Services\MyService;
 
+$service = new MyService();
+$service->run("valaki");
 ?>
