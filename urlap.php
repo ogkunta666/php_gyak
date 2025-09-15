7b5151fb59cbb56d26fda4d4803a8ed1c9078890
ogkunta666 <markizoli06@gmail.com>
2025-09-15 08:29:36 +0200

    jelszo

--- urlap.php
+++ urlap.php
@@ -1,14 +1,17 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name'] ?? 'Név nélkül');
-    echo "Üdvözöllek, " . $name . "!";
+    $password = htmlspecialchars($_POST['password'] ?? 'Nincs megadva');
+    echo "Üdvözöllek, " . $name . "! A jelszavad: " . $password;
 } else {
     
     ?>
     <h1>Űrlapkezelés</h1>
-    <form method="post">
+    <form action="" method="post">
         <label for="name">Név:</label>
         <input type="text" id="name" name="name">
+        <label for="password">Jelszó:</label>
+        <input type="password" id="password" name="password">
         <input type="submit" value="Küldés">
     </form>
     <?php
