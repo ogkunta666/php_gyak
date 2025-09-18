fb726f859c11a0bc5b0c63391b4fb787c7f672a4
ogkunta666 <markizoli06@gmail.com>
2025-09-18 09:26:50 +0200

    xd

---
+++ server.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>server tomb informacioi</h2>
+    <ul>
+        <li><strong>Kérés módja: <?php echo $_SERVER['REQUEST_METHOD']; ?></strong></li>
+        <li><strong>Kért url: <?php echo $_SERVER['REQUEST_URI']; ?></strong></li>
+    </ul>
+</body>
+</html>
