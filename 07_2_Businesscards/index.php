73fb2fb220745586504e5d5d9b6ef58433a81719
zwwxxxxx <126754383+ogkunta666@users.noreply.github.com>
2025-10-06 09:27:29 +0200

    Add files via upload

--- 07_2_Businesscards/index.php
+++ 07_2_Businesscards/index.php
@@ -1,5 +1,8 @@
 <?php
 
+//Controller
+
+
 // Egyszerű autoloader a kód betöltéséhez a mappaszerkezet alapján.
 spl_autoload_register(function ($class) {
     $file = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
@@ -8,5 +11,21 @@ spl_autoload_register(function ($class) {
     }
 });
 
-echo "szia";
+use App\Models\BusinessCard;
+use App\Services\CardManager;
+//use PDO;
+
+//Adatbázis konfig (.env)
+$dbHost = 'localhost';
+$dbName = 'business_cards';
+$dbUser = 'root';
+$dbPass = '';
+
+try {
+    $pdo = new PDO ("mysql:host={$dbHost};dbname={$dbName}", $dbUser, $dbPass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Sikeres csatlakozás az adatbázishoz!<br>";
+} catch (PDOException $ex) {
+    die("Adatbázis kapcsolat hiba: " . $ex->getMessage());
+}
 ?>
